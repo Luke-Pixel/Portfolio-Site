56d3067b56c6328bce1c73b64daeb776ac5e8f44
Luke Tyson <lukeool76@gmail.com>
2021-03-23 20:31:29 +0000

    Added system theme setting

--- script.js
+++ script.js
@@ -14,15 +14,41 @@ for (var i=0; themeDots.length > i; i++){
 	})
 }
 
+//check for chamges in system light / dark mode
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setTheme(theme)
+});
 
+//set theme on user update
 function setTheme(mode){
     if(mode == 'light'){
-        document.getElementById('theme-style').href= 'default.css'
+        setLight()
     }
 
     if(mode == 'dark'){
-        document.getElementById('theme-style').href= 'dark.css'
+        setDark()
     }
 
+    //if sysytem theme selected check system theme
+    if(mode == 'system'){
+        let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if(matched){
+            setDark()
+        }else{
+            setLight()
+        }
+    }
+
+    //update stored theme 
     localStorage.setItem('theme', mode)
+}
+
+//set dark mode
+function setDark(){
+    document.getElementById('theme-style').href= 'dark.css'
+}
+
+//set light mode
+function setLight(){
+    document.getElementById('theme-style').href= 'default.css'
 }
